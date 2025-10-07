e5d74ded0b543cc528ff1fcdcca2dbd9ae24b25a
hus0w <hsynozudogru@outlook.com>
2025-10-07 22:09:07 +0200

    Initialisation du backend avec Spring Boot

---
+++ backend/src/test/java/be/hus0w/five_organizer/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hus0w.five_organizer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
