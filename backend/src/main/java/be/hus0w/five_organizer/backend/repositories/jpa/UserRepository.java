d9f780b3183bfc437c5fe8889fd79b693f1323cf
Hus0w <huseyin.ozudogru@outlook.com>
2026-02-06 00:06:21 +0100

    wip2

--- backend/src/main/java/be/hus0w/five_organizer/backend/repositories/jpa/UserRepository.java
+++ backend/src/main/java/be/hus0w/five_organizer/backend/repositories/jpa/UserRepository.java
@@ -14,5 +14,4 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByActivatedTrue();
     @Query("SELECT u FROM User u WHERE u.email = :email")
     Optional<User> findUserByEmail(@Param("email") String email);
-    //wip
 }
